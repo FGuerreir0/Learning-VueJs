3e6b549e837cf6a8288533030cf64d60dc38e562
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-08-12 13:24:57 +0100

    Adding some new codealongs

---
+++ CodeAlongs/vue-components-comunication/src/components/FilmesLista.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="row">
+    <!-- coluna 1 -->
+    <div class="col-8">
+      <h2>Filmes</h2>
+
+      <ul class="list-group list-group-flush">
+        <FilmesListaIten
+          v-for="filme in filmes"
+          :key="filme.id"
+          :filme="filme"
+          :class="aplicarActive(filme)"
+          @selecionarFilme="filmeselecionado = $event"
+        />
+        <!--MODO COM FOR EACH, GENERO DE MAP NO REACT
+        <FilmesListaIten
+          v-for="filme in filmes"
+          :key="filme.id"
+          v-bind="filme"
+        />
+-->
+        <!--
+          <FilmesListaIten titulo="Vingadores"/> MODO SIMPLES
+        <FilmesListaIten titulo="Ironman"/>
+        <FilmesListaIten titulo="Pantera Negra"/>
+-->
+      </ul>
+    </div>
+
+    <!-- coluna 2 -->
+    <div class="col-4">
+      <FilmesListaItenInfo />
+    </div>
+  </div>
+</template>
+
+<script>
+import FilmesListaIten from "./FilmesListaIten.vue";
+import FilmesListaItenInfo from "./FilmesListaItenInfo.vue";
+
+export default {
+  components: {
+    FilmesListaIten,
+    FilmesListaItenInfo
+  },
+  data() {
+    return {
+      filmes: [
+        { id: 1, titulo: "Vingadores", ano: 2010 },
+        { id: 2, titulo: "Ironman", ano: 1999 },
+        { id: 3, titulo: "Pantera Negra", ano: 2003 }
+      ],
+      filmeselecionado: undefined
+    };
+  },
+  methods:{
+aplicarActive(filmeIterado){
+return{
+  active: this.filmeselecionado && this.filmeselecionado.id === filmeIterado.id
+}
+}
+  }
+};
+</script>
---
+++ CodeAlongs/vue-components-comunication/src/components/FilmesListaIten.vue
@@ -0,0 +1,39 @@
+<template>
+  <li class="list-group-item">
+    <!--<span>{{ titulo }}</span> FORMA SEM CONCATENAÇÃO-->
+    <!--<span>{{ tituloConcac }} | {{ ano }}</span>-->
+    <span>{{ filme.titulo }} | {{ filme.ano }}</span>
+
+    <button @click="selecionar" class="btn btn-success float-right">
+      Seleccionar
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    filme: {
+      type: Object,
+      required: true
+    }
+    /*
+    titulo: { type: String, required: true },
+    ano: { type: Number, required: true }
+  },
+  computed: {
+    tituloConcac() {
+      return `Título: ${this.titulo}`;
+    }*/
+  },
+  methods: {
+    selecionar() {
+      this.$emit("selecionarFilme", this.filme);
+    }
+  }
+  /* LIFECYCLE CREATED didmount no react
+    created() {
+        console.log('Titulo a adicionar:', this.titulo);
+    }*/
+};
+</script>
