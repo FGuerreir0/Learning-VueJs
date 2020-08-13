156b9763c260fc178dfbadb7c5d2cbfb0b7fc8df
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-08-13 18:29:43 +0100

    Starting with Slots

---
+++ CodeAlongs/vue-advanced-components/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app" class='container'>
+    <h1>Advanced Vue Components</h1>
+      <Post>
+        <h3 slot="head"> Passing custome html data</h3>
+
+        <template slot="body">
+        <p>Learning named slots</p>
+        <span>...</span>
+        </template>
+
+         <template>
+        <p>This is a default slot</p>
+        </template>
+
+        <small slot="footer">by Fabio Guerreiro</small>
+
+        <template slot="button">
+          Details...
+        </template>
+      </Post>
+  </div>
+  
+</template>
+
+<script>
+import Post from './components/Post'
+
+export default {
+  components:{
+    Post
+  }
+}
+</script>
+
+<style scoped>
+.container{
+  width:960px;
+  margin:auto;
+}
+</style>
---
+++ CodeAlongs/vue-advanced-components/src/components/Post.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="post">
+  <div class="post-head">
+<slot name='head'></slot>
+  </div>
+
+  <div class="post-body">
+<slot name='body'></slot>
+  </div>
+
+  <div class="post-body">
+    <slot></slot>
+  </div>
+
+  <div class="post-footer">
+<slot name='footer'></slot>
+<a href="#" class="link">
+  <!-- SE NÃO PASSAR NENHUMA INFORMAÇÃO PARA A SLOT APARECERÁ (default link) -->
+  <slot name="button">Default Link...</slot>
+  </a>
+  </div>
+</div>
+</template>
+
+
+<style scoped>
+.post {
+  margin: 15px auto;
+}
+
+.post-head {
+  padding: 5px 12px;
+  background-color: #eeeeee;
+}
+
+.post-body{
+  font-size: 16px;
+}
+
+.post-footer{
+  font-style: italic;
+}
+
+.link{
+  float:right;
+  
+}
+</style>
