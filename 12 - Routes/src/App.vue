a21eac4f7e290eec077bddd916d2fc2093ed44fa
Fabio Guerreiro <ffsguerreiro@hotmail.com>
2020-11-28 18:58:40 +0000

    Start studying vue routes

---
+++ 12 - Routes/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div class="jumbotron">
+      <h1 class="display-4">Vue Router</h1>
+      <p class="lead">Adding Routes to SPA</p>
+    </div>
+    <div class="container">
+      <router-link to="/" class="btn btn-info mb-2 mr-2">Home</router-link>
+
+      <router-link :to="{ path: '/contacts' }" class="btn btn-info mb-2 mr-2">Contacts</router-link>
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
